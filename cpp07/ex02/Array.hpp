2c166fcc15daf999d523e0886b191cccc785a071
Abdellah Moukhless <amoukhle@e1r5p9.1337.ma>
2024-01-11 22:57:59 +0100

    add 07 ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,31 +1,69 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+
 template <typename T> class Array {
 	private:
-		T element;
+		T *element;
+		unsigned int s;
 	public:
 		Array() {
-			new Array[];
+			s = 0;
+			element = new T[s];
 		}
 		Array(unsigned int n) {
-			new Array[n];
+			if (n > 0) {
+				element = new T[n];
+				for (unsigned int i = 0; i < n; i++) {
+					element[i] = 0;
+				}
+				s = n;
+			}
+			else {
+				element = NULL;
+				s = 0;
+			}
 		}
 
 		~Array() {
-
+			delete[] element;
 		}
 
 		Array(const Array& copy) {
-			
+			s = copy.s;
+			element = new T[s];
+			*this = copy;
 		}
 
 		Array& operator=(const Array& obj) {
 			if (this != &obj)
 			{
-				
+				delete[] element;
+				s = obj.s;
+				element = new T[s];
+				for (unsigned int i = 0; i < obj.s; i++) {
+					element[i] = obj.element[i];
+				}
 			}
 			return (*this);
+		}
+
+		class OutOfRangeException : public std::exception{
+			public:
+				const char* what() const throw() {
+					return("Index out of range");
+				}
+		};
+
+		T& operator[](unsigned int index) {
+			if (index >= s)
+				throw OutOfRangeException();
+			return (element[index]);
+		}
+
+		int size() const {
+			return (s);
 		}
 };
 
